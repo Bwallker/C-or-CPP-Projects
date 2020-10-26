4ec43c554cdb6987a9cf7789feff45dc3b75825f
bwallker <benbwall@gmail.com>
2020-10-26 16:21:27 +0200

    d

--- C-or-CPP-Projects/Golden ratio numbers/Source.cpp
+++ C-or-CPP-Projects/Golden ratio numbers/Source.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 string addBigNumbers(string number1, string number2) {
 	string sum = "";
-	unsigned long long int i = 0;
+	unsigned long long int i = std::max(number1.length(), number2.length()) - 1;
 	bool carry = false;
-	while (i < number1.length() || i < number2.length()) {
+	do {
 		int intNumber1 = 0;
 		int intNumber2 = 0;
-		if (i < number1.length()) {
+		if (i <= number1.length()-1) {
 			intNumber1 = number1[i] - '0';
 		}
-		if (i >= number2.length()) {
+		if (i <= number2.length()-1) {
 			intNumber2 = number2[i] - '0';
 		}
 		int intSum = intNumber1 + intNumber2;
@@ -18,7 +19,7 @@ string addBigNumbers(string number1, string number2) {
 			intSum++;
 		}
 
-		if (i = std::max(number1.length(), number2.length()) - 1 & intSum > 9) {
+		if (intSum > 9 & i == 0) {
 			sum.std::string::insert(0, "1");
 		}
 		else if (intSum > 9) {
@@ -28,24 +29,35 @@ string addBigNumbers(string number1, string number2) {
 		else {
 			carry = false;
 		}
-		if ()
-			sum.std::string::append(intSum);
-
-
-		++i;
-	}
+		if (i == std::max(number1.length(), number2.length()) - 1) {
+			sum = std::to_string(intSum);
+		}
+		else if (i != 0) {
+			sum.std::string::insert(sum.length() - 1, std::to_string(intSum));
+		}
+		else {
+			sum.std::string::insert(sum.length() - 2, std::to_string(intSum));
+		}
+		if (i > 0) {
+			--i;
+		}
+		else {
+			break;
+		}
+	} while (i > 0);
 	return sum;
 }
-void addLastTwo(int number1, int number2, int limit) {
+void addLastTwo(string number1, string number2, int limit) {
 	int i = 0;
-	unsigned long long int sum = 0;
+	string sum = "";
 	while (i < limit) {
-		sum = number1 + number2;
+		sum = addBigNumbers(number1, number2);
 		cout << i << ", " << sum << '\n';
 		number2 = number1;
 		number1 = sum;
 		i++;
 	}
+	std::cin.ignore();
 }
 int main() {
 	std::cout << "What are the two starting numbers?" << '\n';
@@ -63,7 +75,8 @@ int main() {
 		startingNumber1 = startingNumber2;
 		startingNumber2 = temp;
 	}
-
-	addLastTwo(startingNumber1, startingNumber2, limit);
+	string startingNumber1string = to_string(startingNumber1);
+	string startingNumber2string = to_string(startingNumber2);
+	addLastTwo(startingNumber1string, startingNumber2string, limit);
 	return 0;
 }
