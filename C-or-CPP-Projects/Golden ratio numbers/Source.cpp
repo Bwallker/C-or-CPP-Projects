24feb027bac17146221099d63f21628f74cb3e4e
bwallker <benbwall@gmail.com>
2020-11-13 10:53:37 +0200

    jjnjn

--- C-or-CPP-Projects/Golden ratio numbers/Source.cpp
+++ C-or-CPP-Projects/Golden ratio numbers/Source.cpp
@@ -3,24 +3,29 @@
 using namespace std;
 string addBigNumbers(string number1, string number2) {
 	string sum = "";
-	unsigned long long int i = std::max(number1.length(), number2.length()) - 1;
 	bool carry = false;
-	do {
+	const unsigned long long int startingNumber1Length = number1.length();
+	const unsigned long long int startingNumber2Length = number2.length();
+	unsigned long long int number1Length = startingNumber1Length;
+	unsigned long long int number2Length = startingNumber2Length;
+	while (true) {
 		int intNumber1 = 0;
 		int intNumber2 = 0;
-		if (i <= number1.length()-1) {
-			intNumber1 = number1[i] - '0';
+		if (number1Length != 0) {
+			intNumber1 = number1[number1Length - 1] - '0';
+			number1 = number1.erase(number1Length - 1);
 		}
-		if (i <= number2.length()-1) {
-			intNumber2 = number2[i] - '0';
+		if (number2Length != 0) {
+			intNumber2 = number2[number2Length - 1] - '0';
+			number2 = number2.erase(number2Length - 1);
 		}
 		int intSum = intNumber1 + intNumber2;
 		if (carry) {
 			intSum++;
 		}
-
-		if (intSum > 9 & i == 0) {
-			sum.std::string::insert(0, "1");
+		bool check = false;
+		if (intSum > 9 & (number1Length == 0 & number2Length == 0)) {
+			bool check = true;
 		}
 		else if (intSum > 9) {
 			intSum -= 10;
@@ -29,30 +34,39 @@ string addBigNumbers(string number1, string number2) {
 		else {
 			carry = false;
 		}
-		if (i == std::max(number1.length(), number2.length()) - 1) {
+		if (number1Length == startingNumber1Length || number2Length == startingNumber2Length) {
 			sum = std::to_string(intSum);
 		}
-		else if (i != 0) {
-			sum.std::string::insert(sum.length() - 1, std::to_string(intSum));
+		else if (intSum != 0 || (number1Length != 0 || number2Length != 0)) {
+			sum.std::string::insert(0, std::to_string(intSum));
 		}
-		else {
-			sum.std::string::insert(sum.length() - 2, std::to_string(intSum));
+		if (check) {
+			sum.std::string::insert(0, "1");
 		}
-		if (i > 0) {
-			--i;
-		}
-		else {
+		if (number1Length == 0 & number2Length == 0) {
 			break;
 		}
-	} while (i > 0);
+		if (number1Length > 0) {
+			number1Length--;
+		}
+		if (number2Length > 0) {
+			number2Length--;
+		}
+	}
 	return sum;
 }
 void addLastTwo(string number1, string number2, int limit) {
+	bool check = false;
+	if (limit < 0) {
+		check = true;
+	}
 	int i = 0;
 	string sum = "";
-	while (i < limit) {
+	cout << 1 << ", " << number1 << '\n';
+	cout << 2 << ", " << number2 << '\n';
+	while (i < limit || check) {
 		sum = addBigNumbers(number1, number2);
-		cout << i << ", " << sum << '\n';
+		cout << i+3 << ", " << sum << '\n';
 		number2 = number1;
 		number1 = sum;
 		i++;
@@ -68,13 +82,8 @@ int main() {
 	cin >> startingNumber1;
 	cout << "Choose the second starting number" << '\n';
 	cin >> startingNumber2;
-	cout << "What do you want the stopping point to be?" << '\n';
+	cout << "What do you want the stopping point to be? Type a negative number to have no limit" << '\n';
 	cin >> limit;
-	if (startingNumber1 > startingNumber2) {
-		int temp = startingNumber1;
-		startingNumber1 = startingNumber2;
-		startingNumber2 = temp;
-	}
 	string startingNumber1string = to_string(startingNumber1);
 	string startingNumber2string = to_string(startingNumber2);
 	addLastTwo(startingNumber1string, startingNumber2string, limit);
